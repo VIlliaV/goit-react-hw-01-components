92a362d30b485e5a1b1ff1f24129ced6dc31ebdd
Illia <illia.vasiliev@gmail.com>
2023-03-07 20:22:08 +0200

    done 1

--- src/components/Profile/Porfile.styled.js
+++ src/components/Profile/Porfile.styled.js
@@ -1,8 +1,36 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  background-color: f0f;
-  width: 200px;
-  font-size: 20px;
+  width: 250px;
+  font-size: 30px;
   text-align: center;
+  background-color: #f3ebeb;
+  padding: 5px;
+  & .description {
+    padding: 5px;
+  }
+  & .location,
+  .tag {
+    padding-top: 5px;
+    font-size: 20px;
+    color: #484343;
+  }
+  & .stats {
+    display: flex;
+    justify-content: space-around;
+    background-color: #cec6c6;
+
+    & li {
+      display: flex;
+      width: 100%;
+      flex-direction: column;
+      font-size: 12px;
+      border: 1px solid black;
+      color: #484343;
+      & .label {
+        font-weight: bold;
+        color: black;
+      }
+    }
+  }
 `;
